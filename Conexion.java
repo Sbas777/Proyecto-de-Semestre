a6c3b683682d752f5f07be2fb183568eda3f1f24
Sbas777 <mestrojefe11@gmail.com>
2026-05-25 18:11:26 -0500

    Update Conexion.java

--- Conexion.java
+++ Conexion.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:postgresql://localhost:5432/supermercado";
+    private static final String URL = "jdbc:postgresql://localhost:5432/Supermercado";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
 
