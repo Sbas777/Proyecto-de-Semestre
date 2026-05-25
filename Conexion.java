0d3afb5fe481ef15f264340b6ab72c52f4168b2c
Sbas777 <mestrojefe11@gmail.com>
2026-05-25 18:00:06 -0500

    Create Conexion.java

---
+++ Conexion.java
@@ -0,0 +1,29 @@
+package com.elahorro.conexion;
+
+/**
+ *
+ * @author famil
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:postgresql://localhost:5432/supermercado";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection conectar() {
+        Connection cn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            cn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("¡Conexión exitosa a la base de datos Supermercado!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: No se encontró el driver de PostgreSQL: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return cn;
+    }
+}
